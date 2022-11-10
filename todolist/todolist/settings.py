219ce91df6603eedac981bbae338a0ba5a8f8cc5
jagernau <stepanenkov25@gmail.com>
2022-11-10 11:13:50 +0200

    new20

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import environ
-from pathlib import Path
 import os
 
 
@@ -36,8 +35,6 @@ DEBUG = env('DEBUG')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
